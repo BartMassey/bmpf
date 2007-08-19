bd140600f20bb27e4cede2586074bf113e39983a
Bart Massey <bart@cs.pdx.edu>
2007-08-18 22:06:28 -0700

    added weight presort; actually costs

--- bpf.c
+++ bpf.c
@@ -15,13 +15,17 @@
 typedef struct { double x, y; } ccoord;
 typedef struct { double r, t; } acoord;
 typedef struct { ccoord posn; acoord vel; } state;
-typedef state *resample(double);
+typedef struct { state state; double weight; } particle_info;
+
+typedef particle_info *resample(double);
 
 static resample resample_naive, resample_optimal;
 
 static const double nsecs = 100;
 static const double dt = 0.1;
+
 int nparticles = 100;
+int sort = 0;
 static resample *resampler = resample_naive;
 
 static double avar = M_PI / 16;
@@ -59,23 +63,21 @@ static void update_state(state *p, double dt) {
 
 static state vehicle = { {0, 0}, {10, 0} };
 
-static state *particle_states[2];
+static particle_info *particle_states[2];
 static int which_particle;
-static state *particle;
-static double *weight;
+static particle_info *particle;
 
 void init_particles(void) {
     int i;
     for (i = 0; i < 2; i++)
 	particle_states[i] = malloc(nparticles * sizeof(*particle_states[0]));
-    weight = malloc(nparticles * sizeof(*weight));
     which_particle = 0;
     particle = particle_states[0];
     for (i = 0; i < nparticles; i++) {
-	particle[i].posn.x = abs(gaussian(1));
-	particle[i].posn.y = abs(gaussian(1));
-	particle[i].vel.r = abs(gaussian(1));
-	particle[i].vel.t = uniform() * M_PI_2;
+	particle[i].state.posn.x = abs(gaussian(1));
+	particle[i].state.posn.y = abs(gaussian(1));
+	particle[i].state.vel.r = abs(gaussian(1));
+	particle[i].state.vel.t = uniform() * M_PI_2;
     }
 }
 
@@ -98,54 +100,54 @@ double gprob(double delta, double sd) {
     return exp(-(delta * delta * sd));
 }
 
-double gps_prob(state *particle, ccoord *gps) {
-    double px = gprob(particle->posn.x - gps->x, pvar);
-    double py = gprob(particle->posn.y - gps->y, pvar);
+double gps_prob(state *s, ccoord *gps) {
+    double px = gprob(s->posn.x - gps->x, pvar);
+    double py = gprob(s->posn.y - gps->y, pvar);
     /* XXX is this right? */
     return px * py;
 }
 
-double imu_prob(state *particle, acoord *imu) {
-    double pr = gprob(particle->vel.r - imu->r, pvar);
-    double pt = gprob(particle->vel.t - imu->t, avar);
+double imu_prob(state *s, acoord *imu) {
+    double pr = gprob(s->vel.r - imu->r, pvar);
+    double pt = gprob(s->vel.t - imu->t, avar);
     /* XXX is this right? */
     return pr * pt;
 }
 
-double sum(double a[]) {
+double sum_weights(void) {
     int i;
-    double t = a[0];
+    double t = particle[0].weight;
     for (i = 1; i < nparticles; i++)
-	t += a[i];
+	t += particle[i].weight;
     return t;
 }
 
-int argmax(double a[]) {
+int argmax_weight(void) {
     int i = 0, j;
     for (j = 1; j < nparticles; j++)
-	if (a[j] > a[i])
+	if (particle[j].weight > particle[i].weight)
 	    i = j;
     return i;
 }
 
-state weighted_sample(double scale) {
+particle_info *weighted_sample(double scale) {
     int i;
     double w = uniform() * scale;
     double t = 0;
     for (i = 0; i < nparticles; i++) {
-	t += weight[i];
+	t += particle[i].weight;
 	if (t >= w)
-	    return particle[i];
+	    return &particle[i];
     }
     fprintf(stderr, "total %g < target %g\n", t, w);
     abort();
 }
 
-state *resample_naive(double scale) {
-    state *newp = particle_states[!which_particle];
+particle_info *resample_naive(double scale) {
+    particle_info *newp = particle_states[!which_particle];
     int i;
     for (i = 0; i < nparticles; i++)
-        newp[i] = weighted_sample(scale);
+        newp[i] = *weighted_sample(scale);
     return newp;
 }
 
@@ -153,14 +155,14 @@ double nform(int n) {
     return 1.0 - pow(uniform(), 1.0 / (n + 1));
 }
 
-state *resample_optimal(double scale) {
-    state *newp = particle_states[!which_particle];
+particle_info *resample_optimal(double scale) {
+    particle_info *newp = particle_states[!which_particle];
     double u0 = nform(nparticles - 1) * scale;
     int i, j = 0;
     double t = 0;
     for (i = 0; i < nparticles; i++) {
-        while (t + weight[j] < u0 && j < nparticles)
-	    t += weight[j++];
+        while (t + particle[j].weight < u0 && j < nparticles)
+	    t += particle[j++].weight;
 	assert(j < nparticles);
 	newp[i] = particle[j];
 	u0 = u0 + (scale - u0) * nform(nparticles - i - 1);
@@ -168,24 +170,40 @@ state *resample_optimal(double scale) {
     return newp;
 }
 
+inline static int sgn(double x) {
+    if (x < 0)
+	return -1;
+    if (x > 0)
+	return 1;
+    return 0;
+}
+
+static int cmp_weight(const void *w1, const void *w2) {
+    const particle_info *p1 = w1;
+    const particle_info *p2 = w2;
+    return -sgn(p1->weight - p2->weight);
+}
+
 state bpf_step(ccoord *gps, acoord *imu, double dt) {
     int i;
-    state *newp;
+    particle_info *newp;
     /* update particles */
     for (i = 0; i < nparticles; i++)
-	update_state(&particle[i], dt);
+	update_state(&particle[i].state, dt);
     /* do probabilistic weighting */
     for (i = 0; i < nparticles; i++) {
-	double gp = gps_prob(&particle[i], gps);
-	double ip = imu_prob(&particle[i], imu);
-        weight[i] = gp * ip;
+	double gp = gps_prob(&particle[i].state, gps);
+	double ip = imu_prob(&particle[i].state, imu);
+        particle[i].weight = gp * ip;
     }
     /* get normalizing constant */
-    double tweight = sum(weight);
+    double tweight = sum_weights();
     /* resample */
+    if (sort)
+	qsort(particle, nparticles, sizeof(particle[0]), cmp_weight);
     newp = resampler(tweight);
     /* find max weighted */
-    state best = particle[argmax(weight)];
+    state best = particle[argmax_weight()].state;
     /* complete */
     particle = newp;
     which_particle = !which_particle;
@@ -214,6 +232,12 @@ int main(int argc, char **argv) {
 	    resampler = resample_naive;
 	else if (!strcmp(argv[2], "optimal"))
 	    resampler = resample_optimal;
+	else
+	    abort();
+    }
+    if (argc > 3) {
+	if (!strcmp(argv[3], "sort"))
+	    sort = 1;
 	else
 	    abort();
     }
