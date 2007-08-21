967ee60ee4d2ec0e6a044c7045dd24debc4949e4
Bart Massey <bart@cs.pdx.edu>
2007-08-20 23:48:49 -0700

    improved and checked erfinv approximation

--- apperf.m
+++ apperf.m
@@ -1,15 +1,35 @@
-format long;
-x = [0:0.01:0.9];
+function [x,z] = horner(lower,step,upper,degree)
+  x = [lower:step:upper];
+  y = erfinv(x);
+  fit = polyfit(x,y,degree);
+  z = polyval(fit,x);
+  printf("if (x <= %g)\n", upper);
+  printf("\treturn %.9f + x * ", fit(degree + 1));
+  for i = 2:degree
+    printf("(%.9f + x * ", fit(degree - i + 2));
+  endfor;
+  printf("%.9f", fit(1));
+  for i = 2:degree
+    printf(")");
+  endfor;
+  printf(";\n");
+endfunction;
+
+[x1,z1] = horner(0,0.001,0.4,1);
+[x2,z2] = horner(0.4,0.001,0.9,2);
+[x3,z3] = horner(0.9,0.001,0.99,3);
+[x4,z4] = horner(0.99,0.0001,0.999,3);
+x = [x1,x2,x3,x4];
 y = erfinv(x);
-polyfit(x,y,3)
-x = [0.9:0.001:0.99];
-y = erfinv(x);
-polyfit(x,y,3)
-#x = [0.99:0.0001:0.999];
-#y = erfinv(x);
+z = [z1,z2,z3,z4];
+plot(x,y,x,z);
+
 #function y = F(x,p)
 #  y = p(1) + p(2) * exp(p(3) * x);
 #endfunction;
-#leasqr(x(:),y(:),[1 1 1],"F",0.0001,20000,ones(length(y),1), \
-#       0.01*ones(3),"dfdp", \
-#       [0.0001*ones(3,1), 0.1*ones(3,1)])
+#x = [0.999:0.00001:0.99999];
+#y = erfinv(x);
+#pin = [1.8,0.005,450];
+#leasqr(x(:),y(:),pin,"F",0.001,20000,ones(length(y),1), \
+#       0.01*ones(length(pin)),"dfdp", \
+#       [0.0001*ones(length(pin),1), 0.1*ones(length(pin),1)])
--- erfinv.h
+++ erfinv.h
@@ -2,11 +2,11 @@
    ALL RIGHTS RESERVED */
 
 /* Approximate normalized inverse error function.  Uses
-   piecewise cubic least-squares approximations obtained
-   using GNU Octave, then hand-tweaks them.  The residual is
-   a hand-tweaked exponential fit, and is horrible.  Error
-   is significant; this is intended only to be used in a
-   fast Gaussian-distributed PRNG, in applications where the
+   piecewise polynomial least-squares approximations
+   obtained using GNU Octave.  The residual is a
+   hand-tweaked exponential fit, and is horrible.  Error is
+   significant; this is intended only to be used in a fast
+   Gaussian-distributed PRNG, in applications where the
    error is not important. */
 
 #ifndef _ERFINV_H
@@ -16,13 +16,14 @@
 
 static float erfinv(float x) {
     assert(0.0 <= x && x < 1.0);
+    if (x <= 0.4)
+        return -0.003241691 + x * 0.922235728;
     if (x <= 0.9)
-	return  x * (1.1117646622106996 + x *
-		  (x * 1.1887663471256378 - 0.8908018914137187));
+        return 0.369856315 + x * (-0.598012907 + x * 1.602813513);
     if (x <= 0.99)
-        return  x * (2257.764033265176 + x *
-	          (x * 883.953153922680 - 2444.745829241307)) -
-	            694.999708131834343;
+        return -694.991315729 + x * (2257.764033265 + x * (-2444.745829241 + x * 883.953153923));
+    if (x <= 0.999)
+        return -717260.870032080 + x * (2168591.882440044 + x * (-2185555.532633572 + x * 734226.973095943));
     return 1.786158859307387 + 0.004 * expf((x - 0.99) * 600);
 }
 
