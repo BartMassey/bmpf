4f114710f8841bb13640054da96124c691c4f74d
Bart Massey <bart@cs.pdx.edu>
2007-08-25 13:13:32 -0700

    fixed up benchmarking scripts; sadly, had to add nickle dependency

--- benchmark.sh
+++ benchmark.sh
@@ -5,25 +5,28 @@
 # [This program is licensed under the GPL version 2 or GPL version 3 or later.]
 # Please see the file COPYING in the source
 # distribution of this software for license terms.
-for a in optimal naive logm naivesort logmsort
+for a in optimal logm logmsort naivesort naive
 do
-  t=100
+  p=100
   incr=100
-  while [ $t -le 10000 ]
+  while [ $p -le 150000 ]
   do
-    if [ $t -ge 500 ] ; then incr=500 ; fi
-    (time ./bpf $t $a >benchtmp/$a-$t.dat) 2>benchtmp/$a-$t.time
-    t=`expr $t + $incr`
-  done
-done
-for a in optimal logm logmsort
-do
-  t=10000
-  incr=5000
-  while [ $t -le 150000 ]
-  do
-    if [ $t -ge 50000 ] ; then incr=25000 ; fi
-    (time ./bpf $t $a >benchtmp/$a-$t.dat) 2>benchtmp/$a-$t.time
-    t=`expr $t + $incr`
+    timesfile=benchtmp/$a-$p.time
+    datfile=benchtmp/$a-$p.dat
+    plotfile=benchtmp/$a-$p.plot
+    (time ./bpf $p $a >$datfile) 2>$timesfile
+    t=`awk '$1=="real" {
+       i = index($2, "m")
+       m = substr($2, 1, i - 1)
+       s = substr($2, i + 1, length($2) - i + 1)
+       print 60 * m + s
+       }' < $timesfile`
+    echo $p $t >$plotfile
+    if nickle -e "if($t > 150) exit(0); else exit(1);;" \
+       >/dev/null ; then break ; fi
+    if [ $p -ge 500 ] ; then incr=500 ; fi
+    if [ $p -ge 10000 ] ; then incr=5000 ; fi
+    if [ $p -ge 50000 ] ; then incr=25000 ; fi
+    p=`expr $p + $incr`
   done
 done
--- plottimes.sh
+++ plottimes.sh
@@ -12,17 +12,7 @@ plot \
 EOF
 for a in naive naivesort logmsort logm optimal
 do
-  for f in $a-*.time
-  do
-    n=`echo $f | sed -e "s/^$a-//" -e 's/\.time$//'`
-    t=`awk '$1=="real" {
-       i = index($2, "m")
-       m = substr($2, 1, i - 1)
-       s = substr($2, i + 1, length($2) - i + 1)
-       print 60 * m + s
-       }' < $f`
-    echo $n $t
-  done | sort -n
+  cat $a-*.plot | sort -n
   echo 'e'
 done
 #echo 'pause -1'
