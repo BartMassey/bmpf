1698d7a4579bd7b220a037ae61a6f7b5d6f7a628
Bart Massey <bart@cs.pdx.edu>
2007-08-29 17:33:25 -0700

    added new separate compilation files

---
+++ resample/resample.h
@@ -0,0 +1,24 @@
+#ifndef _RESAMPLE_H
+#define _RESAMPLE_H
+
+#include <ziggurat/random.h>
+#include "../bpf.h"
+
+/* accepts a total weight, an input sample size, an input sample array,
+   an output sample size, an output sample array, and a sort flag */
+/* returns a highest-weighted particle index */
+typedef int resample(double, int, particle_info *, int, particle_info *, int);
+/* accepts a maximum number of particles input / output */
+typedef void init_resample(int, int);
+
+extern init_resample init_resample_logm;
+extern resample resample_naive, resample_optimal,
+       resample_logm, resample_regular;
+
+extern struct resample_info {
+    char *name;
+    init_resample *f_init;
+    resample *f;
+} resamplers[];
+
+#endif
